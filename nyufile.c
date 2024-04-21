42a5d0293e4488104349e2bbded798e67fdb6f67
Manisha Goyal <manisha.goyal1996@gmail.com>
2024-04-21 00:13:03 -0400

    Completed milestone 2

--- nyufile.c
+++ nyufile.c
@@ -5,8 +5,21 @@
 #include <fcntl.h>
 #include <stdint.h>
 
+#pragma pack(push,1)
+typedef struct {
+    uint8_t BS_jmpBoot[3];
+    uint8_t BS_OEMName[8];
+    uint16_t BPB_BytsPerSec;
+    uint8_t BPB_SecPerClus;
+    uint16_t BPB_RsvdSecCnt;
+    uint8_t BPB_NumFATs;
+    uint8_t otherData[19];
+} BootSector;
+#pragma pack(pop)
+
 void print_usage();
-void print_file_system_info(const char *diskImage);
+BootSector readBootSector(const char *diskImage);
+void printFileSystemInfo(BootSector bootSector);
 
 int main(int argc, char *argv[]) {
     int opt;
@@ -63,9 +76,6 @@ int main(int argc, char *argv[]) {
             print_usage();
             return 1;
         }
-        if(i_flag) {
-            print_file_system_info(diskImage);
-        }
     } else if (r_flag) {
         if ((filename == NULL || strlen(filename) == 0) || (s_flag && (sha1 == NULL || strlen(sha1) == 0))) {
             print_usage();
@@ -81,12 +91,13 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    return 0;
-}
+    BootSector bootSector = readBootSector(diskImage);
 
-void print_file_system_info(const char *diskImage) {
-    //add code here
-    printf(diskImage);
+    if(i_flag) {
+        printFileSystemInfo(bootSector);
+    }
+
+    return 0;
 }
 
 void print_usage() {
@@ -95,12 +106,37 @@ void print_usage() {
     printf("  -l                     List the root directory.\n");
     printf("  -r filename [-s sha1]  Recover a contiguous file.\n");
     printf("  -R filename -s sha1    Recover a possibly non-contiguous file.\n");
-    exit(0);
+    exit(EXIT_SUCCESS);
+}
+
+BootSector readBootSector(const char *diskImage) {
+    BootSector bootSector;
+    int fd = open(diskImage, O_RDONLY);
+
+    if (fd == -1) {
+        perror("Error opening disk image");
+        exit(EXIT_FAILURE);
+    }
+
+    if (read(fd, &bootSector, sizeof(BootSector)) != sizeof(BootSector)) {
+        perror("Error reading boot sector");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+    close(fd);
+    
+    return bootSector;
+}
+
+void printFileSystemInfo(BootSector bootSector) {
+    printf("Number of FATs = %d\n", bootSector.BPB_NumFATs);
+    printf("Number of bytes per sector = %d\n", bootSector.BPB_BytsPerSec);
+    printf("Number of sectors per cluster = %d\n", bootSector.BPB_SecPerClus);
+    printf("Number of reserved sectors = %d\n", bootSector.BPB_RsvdSecCnt);
 }
 
 /* References:
-
 https://people.cs.rutgers.edu/~pxk/416/notes/c-tutorials/getopt.html
-
-
+https://www.cs.fsu.edu/~cop4610t/lectures/project3/Week11/Slides_week11.pdf
+https://academy.cba.mit.edu/classes/networking_communications/SD/FAT.pdf
 */
