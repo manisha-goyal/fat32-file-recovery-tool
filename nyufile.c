6df414686859e6457448f3fe279601c1a1a88195
Manisha Goyal <manisha.goyal1996@gmail.com>
2024-04-20 23:09:53 -0400

    Completed milestone 1

---
+++ nyufile.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void print_usage();
+
+int main(int argc, char *argv[]) {
+    int opt;
+    char *filename = NULL;
+    char *sha1 = NULL;
+    int r_flag = 0, R_flag = 0, i_flag = 0, l_flag = 0, s_flag = 0;
+
+    while ((opt = getopt(argc, argv, "ilr:Rs:")) != -1) {
+        switch (opt) {
+            case 'i':
+                i_flag = 1;
+                break;
+            case 'l':
+                l_flag = 1;
+                break;
+            case 'r':
+                r_flag = 1;
+                filename = optarg;
+                break;
+            case 'R':
+                R_flag = 1;
+                filename = optarg;
+                break;
+            case 's':
+                s_flag = 1;
+                sha1 = optarg;
+                break;
+            default:
+                print_usage();
+                return 1;
+        }
+    }
+
+    if ((i_flag && argc != 3) || (l_flag && argc != 3) || (r_flag && R_flag) ||
+        (r_flag && !filename ) || (R_flag && (!filename || !s_flag || !sha1))) {
+        print_usage();
+        return 1;
+    }
+
+    if ((i_flag + l_flag + r_flag + R_flag) != 1) {
+        print_usage();
+        return 1;
+    }
+
+    if (i_flag || l_flag) {
+        if (argc != 3) {
+            print_usage();
+            return 1;
+        }
+    } else if (r_flag) {
+        if ((filename == NULL || strlen(filename) == 0) || (s_flag && (sha1 == NULL || strlen(sha1) == 0))) {
+            print_usage();
+            return 1;
+        }
+    } else if (R_flag) {
+        if (filename == NULL || strlen(filename) == 0 || !s_flag || sha1 == NULL || strlen(sha1) == 0) {
+            print_usage();
+            return 1;
+        }
+    } else {
+        print_usage();
+        return 1;
+    }
+
+    return 0;
+}
+
+void print_usage() {
+    printf("Usage: ./nyufile disk <options>\n");
+    printf("  -i                     Print the file system information.\n");
+    printf("  -l                     List the root directory.\n");
+    printf("  -r filename [-s sha1]  Recover a contiguous file.\n");
+    printf("  -R filename -s sha1    Recover a possibly non-contiguous file.\n");
+    exit(0);
+}
+
+/* References:
+
+https://people.cs.rutgers.edu/~pxk/416/notes/c-tutorials/getopt.html
+
+
+*/
