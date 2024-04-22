45d711546fd46d2211c43c95fb97b9e7d6017386
Manisha Goyal <manisha.goyal1996@gmail.com>
2024-04-22 02:22:38 -0400

    Completed milestone 3

--- nyufile.c
+++ nyufile.c
@@ -3,104 +3,169 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdint.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 #pragma pack(push,1)
-typedef struct {
-    uint8_t BS_jmpBoot[3];
-    uint8_t BS_OEMName[8];
-    uint16_t BPB_BytsPerSec;
-    uint8_t BPB_SecPerClus;
-    uint16_t BPB_RsvdSecCnt;
-    uint8_t BPB_NumFATs;
-    uint8_t otherData[19];
-} BootSector;
+typedef struct BootEntry {
+  unsigned char  BS_jmpBoot[3];     // Assembly instruction to jump to boot code
+  unsigned char  BS_OEMName[8];     // OEM Name in ASCII
+  unsigned short BPB_BytsPerSec;    // Bytes per sector. Allowed values include 512, 1024, 2048, and 4096
+  unsigned char  BPB_SecPerClus;    // Sectors per cluster (data unit). Allowed values are powers of 2, but the cluster size must be 32KB or smaller
+  unsigned short BPB_RsvdSecCnt;    // Size in sectors of the reserved area
+  unsigned char  BPB_NumFATs;       // Number of FATs
+  unsigned short BPB_RootEntCnt;    // Maximum number of files in the root directory for FAT12 and FAT16. This is 0 for FAT32
+  unsigned short BPB_TotSec16;      // 16-bit value of number of sectors in file system
+  unsigned char  BPB_Media;         // Media type
+  unsigned short BPB_FATSz16;       // 16-bit size in sectors of each FAT for FAT12 and FAT16. For FAT32, this field is 0
+  unsigned short BPB_SecPerTrk;     // Sectors per track of storage device
+  unsigned short BPB_NumHeads;      // Number of heads in storage device
+  unsigned int   BPB_HiddSec;       // Number of sectors before the start of partition
+  unsigned int   BPB_TotSec32;      // 32-bit value of number of sectors in file system. Either this value or the 16-bit value above must be 0
+  unsigned int   BPB_FATSz32;       // 32-bit size in sectors of one FAT
+  unsigned short BPB_ExtFlags;      // A flag for FAT
+  unsigned short BPB_FSVer;         // The major and minor version number
+  unsigned int   BPB_RootClus;      // Cluster where the root directory can be found
+  unsigned short BPB_FSInfo;        // Sector where FSINFO structure can be found
+  unsigned short BPB_BkBootSec;     // Sector where backup copy of boot sector is located
+  unsigned char  BPB_Reserved[12];  // Reserved
+  unsigned char  BS_DrvNum;         // BIOS INT13h drive number
+  unsigned char  BS_Reserved1;      // Not used
+  unsigned char  BS_BootSig;        // Extended boot signature to identify if the next three values are valid
+  unsigned int   BS_VolID;          // Volume serial number
+  unsigned char  BS_VolLab[11];     // Volume label in ASCII. User defines when creating the file system
+  unsigned char  BS_FilSysType[8];  // File system type label in ASCII
+} BootEntry;
 #pragma pack(pop)
 
-void print_usage();
-BootSector readBootSector(const char *diskImage);
-void printFileSystemInfo(BootSector bootSector);
+#pragma pack(push,1)
+typedef struct DirEntry {
+  unsigned char  DIR_Name[11];      // File name
+  unsigned char  DIR_Attr;          // File attributes
+  unsigned char  DIR_NTRes;         // Reserved
+  unsigned char  DIR_CrtTimeTenth;  // Created time (tenths of second)
+  unsigned short DIR_CrtTime;       // Created time (hours, minutes, seconds)
+  unsigned short DIR_CrtDate;       // Created day
+  unsigned short DIR_LstAccDate;    // Accessed day
+  unsigned short DIR_FstClusHI;     // High 2 bytes of the first cluster address
+  unsigned short DIR_WrtTime;       // Written time (hours, minutes, seconds
+  unsigned short DIR_WrtDate;       // Written day
+  unsigned short DIR_FstClusLO;     // Low 2 bytes of the first cluster address
+  unsigned int   DIR_FileSize;      // File size in bytes. (0 for directories)
+} DirEntry;
+#pragma pack(pop)
+
+#define ATTR_DIRECTORY 0x10
+#define ATTR_LONG_NAME 0x0f
+#define DELETED_FILE 0xe5
+#define END_OF_DIRECTORY 0x00
+#define END_OF_CLUSTER 0x0ffffff8
+
+void printUsage();
+void printFileSystemInfo(BootEntry *bootEntry);
+void listRootDirectory(BootEntry *bootEntry, char *diskMap);
+char *formatDirEntryName(const unsigned char *dirName);
 
 int main(int argc, char *argv[]) {
     int opt;
     char *filename = NULL;
     char *sha1 = NULL;
-    int r_flag = 0, R_flag = 0, i_flag = 0, l_flag = 0, s_flag = 0;
+    int rFlag = 0, RFlag = 0, iFlag = 0, lFlag = 0, sFlag = 0;
 
     while ((opt = getopt(argc, argv, "ilr:R:s:")) != -1) {
         switch (opt) {
             case 'i':
-                i_flag = 1;
+                iFlag = 1;
                 break;
             case 'l':
-                l_flag = 1;
+                lFlag = 1;
                 break;
             case 'r':
-                r_flag = 1;
+                rFlag = 1;
                 filename = optarg;
                 break;
             case 'R':
-                R_flag = 1;
+                RFlag = 1;
                 filename = optarg;
                 break;
             case 's':
-                s_flag = 1;
+                sFlag = 1;
                 sha1 = optarg;
                 break;
             default:
-                print_usage();
-                return 1;
+                printUsage();
         }
     }
 
     if (optind >= argc) {
-        print_usage();
-        return 1;
+        printUsage();
+    }
+
+    if ((iFlag + lFlag + rFlag + RFlag) != 1) {
+        printUsage();
+    }
+
+    if (iFlag || lFlag) {
+        if (argc != 3) {
+            printUsage();
+        }
+    } else if (rFlag) {
+        if ((filename == NULL || strlen(filename) == 0) || (sFlag && (sha1 == NULL || strlen(sha1) == 0))) {
+            printUsage();
+        }
+    } else if (RFlag) {
+        if (filename == NULL || strlen(filename) == 0 || !sFlag || sha1 == NULL || strlen(sha1) == 0) {
+            printUsage();
+        }
+    } else {
+        printUsage();
     }
 
     char *diskImage = argv[optind];
-
-    if ((i_flag && argc != 3) || (l_flag && argc != 3) || (r_flag && R_flag) ||
-        (r_flag && !filename ) || (R_flag && (!filename || !s_flag || !sha1))) {
-        print_usage();
-        return 1;
+    
+    int fd = open(diskImage, O_RDWR);
+    if (fd == -1) {
+        fprintf(stderr, "Error opening disk image");
+        exit(EXIT_FAILURE);
     }
 
-    if ((i_flag + l_flag + r_flag + R_flag) != 1) {
-        print_usage();
-        return 1;
+    struct stat sb;
+    if (fstat(fd, &sb) == -1) {
+        close(fd);
+        fprintf(stderr, "Error determining file size");
+        exit(EXIT_FAILURE);
     }
 
-    if (i_flag || l_flag) {
-        if (argc != 3) {
-            print_usage();
-            return 1;
-        }
-    } else if (r_flag) {
-        if ((filename == NULL || strlen(filename) == 0) || (s_flag && (sha1 == NULL || strlen(sha1) == 0))) {
-            print_usage();
-            return 1;
-        }
-    } else if (R_flag) {
-        if (filename == NULL || strlen(filename) == 0 || !s_flag || sha1 == NULL || strlen(sha1) == 0) {
-            print_usage();
-            return 1;
-        }
-    } else {
-        print_usage();
-        return 1;
+    char *diskMap = mmap(NULL, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    close(fd);
+    
+    if (diskMap == MAP_FAILED) {
+        fprintf(stderr, "Error mapping disk image");
+        exit(EXIT_FAILURE);
     }
 
-    BootSector bootSector = readBootSector(diskImage);
+    BootEntry *bootEntry = (BootEntry *)diskMap;
+    if (!bootEntry) {
+        fprintf(stderr, "Failed to initialize disk image\n");
+        exit(EXIT_FAILURE);
+    }
 
-    if(i_flag) {
-        printFileSystemInfo(bootSector);
+    if(iFlag) {
+        printFileSystemInfo(bootEntry);
+    }
+
+    if (lFlag) {
+        listRootDirectory(bootEntry, diskMap);
+    } 
+
+    if (diskMap) {
+        munmap(diskMap, sb.st_size);
     }
 
     return 0;
 }
 
-void print_usage() {
+void printUsage() {
     printf("Usage: ./nyufile disk <options>\n");
     printf("  -i                     Print the file system information.\n");
     printf("  -l                     List the root directory.\n");
@@ -109,34 +174,94 @@ void print_usage() {
     exit(EXIT_SUCCESS);
 }
 
-BootSector readBootSector(const char *diskImage) {
-    BootSector bootSector;
-    int fd = open(diskImage, O_RDONLY);
-
-    if (fd == -1) {
-        perror("Error opening disk image");
-        exit(EXIT_FAILURE);
-    }
-
-    if (read(fd, &bootSector, sizeof(BootSector)) != sizeof(BootSector)) {
-        perror("Error reading boot sector");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-    close(fd);
-    
-    return bootSector;
+void printFileSystemInfo(BootEntry *bootEntry) {
+    printf("Number of FATs = %d\n", bootEntry->BPB_NumFATs);
+    printf("Number of bytes per sector = %d\n", bootEntry->BPB_BytsPerSec);
+    printf("Number of sectors per cluster = %d\n", bootEntry->BPB_SecPerClus);
+    printf("Number of reserved sectors = %d\n", bootEntry->BPB_RsvdSecCnt);
 }
 
-void printFileSystemInfo(BootSector bootSector) {
-    printf("Number of FATs = %d\n", bootSector.BPB_NumFATs);
-    printf("Number of bytes per sector = %d\n", bootSector.BPB_BytsPerSec);
-    printf("Number of sectors per cluster = %d\n", bootSector.BPB_SecPerClus);
-    printf("Number of reserved sectors = %d\n", bootSector.BPB_RsvdSecCnt);
+void listRootDirectory(BootEntry *bootEntry, char *diskMap) {
+    int rootCluster = bootEntry->BPB_RootClus;
+    int clusterSize = bootEntry->BPB_SecPerClus * bootEntry->BPB_BytsPerSec;
+    int reservedSecOffset = bootEntry->BPB_RsvdSecCnt * bootEntry->BPB_BytsPerSec;
+    int fatOffset = (bootEntry->BPB_NumFATs * bootEntry->BPB_FATSz32) * bootEntry->BPB_BytsPerSec;
+    int *FAT = (int *)(diskMap + reservedSecOffset);
+    int entriesPerCluster = (int)(clusterSize/sizeof(DirEntry));
+
+    int totalEntries = 0;
+
+    while (rootCluster < END_OF_CLUSTER) {
+        int clusterOffset = ((rootCluster - 2) * clusterSize) + reservedSecOffset + fatOffset;
+        DirEntry *entry = (DirEntry *)(diskMap + clusterOffset);
+        int entryCount = 0;
+
+        while(entryCount < entriesPerCluster && entry->DIR_Name[0] != END_OF_DIRECTORY) {
+            if (entry->DIR_Name[0] == DELETED_FILE || entry->DIR_Attr == ATTR_LONG_NAME) {
+                entry++;
+                continue;
+            }
+
+            char *formattedName = formatDirEntryName(entry->DIR_Name);
+            int startingCluster = ((unsigned int)entry->DIR_FstClusHI << 16) | entry->DIR_FstClusLO;
+
+            if (entry->DIR_Attr == ATTR_DIRECTORY) {
+                printf("%s/ (starting cluster = %u)\n", formattedName, startingCluster);
+            } else if (entry->DIR_FileSize != 0){ 
+                printf("%s (size = %u, starting cluster = %u)\n", formattedName, entry->DIR_FileSize, startingCluster);
+            } else {
+                printf("%s (size = %u)\n", formattedName, entry->DIR_FileSize);
+            }
+
+            if (formattedName != NULL) {
+                free(formattedName);
+            }
+
+            entry++;
+            entryCount++;
+        }
+
+        totalEntries+=entryCount;
+        rootCluster = FAT[rootCluster];
+    }
+    
+    printf("Total number of entries = %d\n", totalEntries);
+}
+
+char *formatDirEntryName(const unsigned char* dirName) {
+    char* formattedName = malloc(13 * sizeof(char));
+    int nameLen = 0, pos = 0;
+
+    while (pos < 8 && dirName[pos] != ' ') {
+        formattedName[nameLen++] = dirName[pos++];
+    }
+
+    int hasExtension = 0;
+    pos = 8;
+    while (pos < 11 && !hasExtension) {
+        if (dirName[pos] != ' ') {
+            hasExtension = 1;
+        }
+        pos++;
+    }
+
+    if (hasExtension) {
+        formattedName[nameLen++] = '.';
+        pos = 8;
+        while (pos < 11 && dirName[pos] != ' ') {
+            formattedName[nameLen++] = dirName[pos++];
+        }
+    }
+
+    formattedName[nameLen] = '\0';
+    return formattedName;
 }
 
 /* References:
 https://people.cs.rutgers.edu/~pxk/416/notes/c-tutorials/getopt.html
 https://www.cs.fsu.edu/~cop4610t/lectures/project3/Week11/Slides_week11.pdf
 https://academy.cba.mit.edu/classes/networking_communications/SD/FAT.pdf
+https://www.rapidtables.com/convert/number/ascii-to-hex.html
+https://people.cs.umass.edu/~liberato/courses/2017-spring-compsci365/lecture-notes/11-fats-and-directory-entries/
+https://averstak.tripod.com/fatdox/dir.htm#atr
 */
